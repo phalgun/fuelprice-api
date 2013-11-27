7bd4d126269e516f3a9a54721e3d710e19120eb4
Phalgun Guduthur <me@phalgun.in>
2013-11-27 18:18:54 +0530

    Handle different cases of city names

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ def petrol_prices_all():
     all_petrol_prices = scrapy.scrape_all_petrol_prices()
     return make_response(jsonify(all_petrol_prices))
 
-
 @app.route('/fuelprice/v1.0/petrol/<string:city_name>', methods=['GET'])
 def petrol_price(city_name):
-    url = urls.petrol_url(city_name)
+    url = urls.petrol_url(city_name.lower())
     price = scrapy.scrape_latest_petrol_price(url)
     return make_response(jsonify({city_name.title() : price}))
     
